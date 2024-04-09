7c12adfde5fe54abbad84336d7d789901eaa2504
2dubu <2dubu.dev@gmail.com>
2024-04-09 11:27:59 +0900

    :tada: :: tada!!

---
+++ UMC-2dubu/UMC-2dubu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UMC-2dubu
+//
+//  Created by 이건우 on 4/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
