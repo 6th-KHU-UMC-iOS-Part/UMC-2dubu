a8be560ab6ceb1dd62d0454ece2352e11fa87ceb
2dubu <2dubu.dev@gmail.com>
2024-04-09 16:28:03 +0900

    :truck: :: change file structure

--- UMC-2dubu/UMC-2dubu/View/Main/MainViewController.swift
+++ UMC-2dubu/UMC-2dubu/View/Main/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  UMC-2dubu
 //
 //  Created by 이건우 on 4/9/24.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
