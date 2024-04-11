0985a642472d7f2c52980547e03d9f19dac02389
2dubu <2dubu.dev@gmail.com>
2024-04-11 10:20:10 +0900

    :art: :: helper file

---
+++ UMC-2dubu/UMC-2dubu/Helpers/Helpers.swift
@@ -0,0 +1,25 @@
+//
+//  Helpers.swift
+//  UMC-2dubu
+//
+//  Created by 이건우 on 4/9/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
+    
+    static let mainColor: UIColor = UIColor(r: 43, g: 193, b: 188)
+}
+
+extension UIView {
+    func roundCorners(cornerRadius: CGFloat, maskedCorners: CACornerMask) {
+        clipsToBounds = true
+        layer.cornerRadius = cornerRadius
+        layer.maskedCorners = CACornerMask(arrayLiteral: maskedCorners)
+    }
+}
