4554093b7639b47d202c3170148ebf33cbb4eefd
2dubu <2dubu.dev@gmail.com>
2024-04-11 18:33:10 +0900

    :bento: :: Add category icon with title

---
+++ UMC-2dubu/UMC-2dubu/View/Main/CategoryCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryCollectionViewCell.swift
+//  UMC-2dubu
+//
+//  Created by 이건우 on 4/11/24.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
